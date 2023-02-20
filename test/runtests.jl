3ce9bf4d60b10da403b54aef5d0b08aee5f1eb28
Paul Manis <pmanis@med.unc.edu>
2023-02-19 21:13:38 -0500

    run tests to reader

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using Test
 @testset "Acq4Reader.jl" begin
     # Write your tests here.
     # test 1: did module load ok?
-    err = Acq4Reader.test_configread()
-    # err = Acq4Reader.test_reader()
+    # err = Acq4Reader.test_configread()
+    err = Acq4Reader.test_reader()
 #@test err == false
 
 end
